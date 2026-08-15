3417ea75163a50d944c7af6c0537cb5817129804
papo1011 <francesco.papini.siena@gmail.com>
2026-08-15 16:04:09 +0200

    feat(tokenizer): add tokenizer.model and init SentencePiece processor class

---
+++ sentencepiece.py
@@ -0,0 +1,9 @@
+class SentencePieceProcessor:
+    def __init__(self):
+        """Process the tokenizer model file and get the usefull infos"""
+
+    def encode():
+        """raw text -> normalize -> BPE encoding -> byte fallback -> token IDs"""
+
+    def decode():
+        """token IDs -> byte handling -> text"""
