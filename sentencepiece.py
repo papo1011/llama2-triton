36c08339e38bce77490d54997d1a86cb4f1affeb
papo1011 <francesco.papini.siena@gmail.com>
2026-08-17 00:13:04 +0200

    feat(sentencepiece): add inspect method to print general struct .model file

--- sentencepiece.py
+++ sentencepiece.py
@@ -6,17 +6,34 @@ import sentencepiece_model_pb2
 class SentencePieceProcessor:
     def __init__(self):
         """Process the tokenizer model file and get the usefull infos"""
+
         model = sentencepiece_model_pb2.ModelProto()
         with open("tokenizer.model", "rb") as f:
             model.ParseFromString(f.read())
 
-        self.pieces: list[string] = []
-        self.scores: list[float] = []
-        self.ids: dict[string, int] = {}
+        self.fields: list[str] = []
+        for field in model.DESCRIPTOR.fields:
+            self.fields.append(field.name)
+            setattr(self, field.name, getattr(model, field.name))
+
+        self.p: list[str] = []  # pieces list
+        self.s: list[float] = []  # scores list
+        self.ids: dict[str, int] = {}
         for id, v in enumerate(model.pieces):
-            self.pieces.append(v.piece)
-            self.scores.append(v.score)
+            self.p.append(v.piece)
+            self.s.append(v.score)
             self.ids[v.piece] = id
+
+    def inspect(self):
+        """print a simple list of the structure of the tokenizer.model file"""
+
+        for field in self.fields:
+            if field == "pieces":
+                continue
+
+            print("-----------------------")
+            print("**", field, "**")
+            print(getattr(self, field))
 
     def encode():
         """raw text -> normalize -> BPE encoding -> byte fallback -> token IDs"""
@@ -26,3 +43,4 @@ class SentencePieceProcessor:
 
 
 sp = SentencePieceProcessor()
+sp.inspect()
