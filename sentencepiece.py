6d1c3545c97b5d567582f059dc4ec61200ce786b
papo1011 <francesco.papini.siena@gmail.com>
2026-08-16 23:01:07 +0200

    feat(sentencepiece): implement SentencePieceProcessor and add model parsing logic

--- sentencepiece.py
+++ sentencepiece.py
@@ -1,9 +1,28 @@
+import string
+
+import sentencepiece_model_pb2
+
+
 class SentencePieceProcessor:
     def __init__(self):
         """Process the tokenizer model file and get the usefull infos"""
+        model = sentencepiece_model_pb2.ModelProto()
+        with open("tokenizer.model", "rb") as f:
+            model.ParseFromString(f.read())
+
+        self.pieces: list[string] = []
+        self.scores: list[float] = []
+        self.ids: dict[string, int] = {}
+        for id, v in enumerate(model.pieces):
+            self.pieces.append(v.piece)
+            self.scores.append(v.score)
+            self.ids[v.piece] = id
 
     def encode():
         """raw text -> normalize -> BPE encoding -> byte fallback -> token IDs"""
 
     def decode():
         """token IDs -> byte handling -> text"""
+
+
+sp = SentencePieceProcessor()
